e051dc8f5d70f6a6121a49ed1327df853aa6d2f5
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-01 15:07:08 +0600

    Updated Printf Solve1.c

--- Printf Practice Problems/Printf Solve1.c
+++ Printf Practice Problems/Printf Solve1.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{   printf("\t\t\t\t \"Misbah Ahmed Araf's \'CSE103\' Printf problems\" \t\n\n\n");
+    printf("\bProblem : Make a code that Shows output just like the pdf\b\nSolutions:\n\n");
+
+
+
+    printf("\n1.\nBangladesh is my country. I love my country\n\n");
+    printf("2.\nThe woods are lovely,dark and deep,\nBut i have promises to keep\nAnd miles to go before i sleep\nAnd miles to go before i sleep.\n\n");
+    printf("3.\n1   \n12  \n123 \n1234\n\n");
+    printf("4.\n*   \n**  \n*** \n****\n*** \n**  \n*   \n\n");
+    printf("5.\n*******\n*     *\n*     *\n*     *\n*     *\n*******\n\n");
+    printf("6.\n   1   \n  121  \n 12321 \n1234321\n\n");
+    printf("7.\n    *     \n  *   *  \n*   *   *\n  *   *   \n    *   \n\n");
+    printf("8.\n*******\n ***** \n  ***  \n   *    \n  ***  \n ***** \n*******\n\n");
+    printf("9.(a)11+22=33\n");
+    int X= 11;
+    int Y= 22;
+    printf("  (b)%d+%d=%d\n\n",X,Y,X+Y);
+    printf("10.(a)10x25=250\n");
+    int p= 10;
+    int q=25;
+    printf("   (b)%dx%d=%d\n\n",p,q,p*q);
+
+    printf("11.(a)6/3=2\n");
+    int A= 6;
+    int B=3;
+    printf("   (b)%d/%d=%d\n\n",A,B,A/B);
+    printf("12.(a)135%%2=1\n");
+    int x=135;
+    int y=2;
+    printf("   (b)%d%%%d=%d\n",x,y,x%y);
+
+
+    printf("______________________________________");
+
+
+
+
+
+
+    return 0;
+}
