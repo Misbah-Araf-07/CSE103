f3e8c5165cad1abc727380609828d1d37d12ea71
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-03 23:45:51 +0600

    Add files via upload

---
+++ Scanf Problems/C scanf Problem Solutions -2 .c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\t\t\t\t \"Misbah Ahmed Araf's  \'CSE103\' Scanf Problems\" \t\n\n\n");
+
+    printf("Problem-3: Write a C program to take a character(c) as input and display it.\n\n");
+
+    char X;
+    printf("Solution:\n\nPlease input a Character\n\n");
+    scanf("%c",&X);
+    printf("Your given Character is: %c\n",X);
+
+
+
+    printf("_____________________________________________________");
+
+    return 0;
+}
