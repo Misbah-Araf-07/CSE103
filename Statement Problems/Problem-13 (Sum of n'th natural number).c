ba03a4107a023d60f670fc87a091b33f4fd4964e
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-12 23:58:43 +0600

    Update Problem-13 (Sum of n'th natural number).c

--- Statement Problems/Problem-13 (Sum of n'th natural number).c
+++ Statement Problems/Problem-13 (Sum of n'th natural number).c
@@ -6,14 +6,13 @@ int main()
 {
     printf("\t\t\t\t\"Misbah Ahmed Araf's Statement problems\"\n\n\n");
     printf("Problem-13:Write a C program that takes an input and sums it up to n'th number.\n\nSolution:\n");
+    int n;
 
-    int nums[]={1,2,3};
-    int n=1;
-        while (n>=1 && n<1000000)
-            printf("%d\n\n",nums[n++]);
-//
-//        for (int n=0;n<10;++n)
-//            printf("%d ",n+1);
+    printf("Please Input n'th number: ");
+    scanf("%d",&n);
+    printf("Summed up Number= %d\n",(n*(n+1))/2);
+
+
 
 
     return 0;
