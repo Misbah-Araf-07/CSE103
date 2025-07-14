639701bd31f600a4978dfe025202bd8a01610aae
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-14 10:59:45 +0600

    Adding For While Do-while Versions

--- Loop/Asking for a number between 1~100 until the perfect input  is given.c
+++ Loop/Asking for a number between 1~100 until the perfect input  is given.c
@@ -4,6 +4,22 @@ int main ()
 {
 
 
+    
+*/ For Loop
+int A;
+    scanf("%d", &A);
+    for (;A<1 ||A>100 ;)
+    {
+    printf("RE ENTER NUMBER ");
+    scanf("%d",&A);
+    }
+    printf("Perfect : %d\n",A);
+*/
+
+    
+
+
+*/ While loop
     int A=0;
     printf("Input a num from 1 to 100  : ");
     scanf("%d",A);
@@ -14,5 +30,29 @@ int main ()
         scanf("%d",&A);
     }
     printf("Perfect : %d",A);
+*/
 
+
+
+    
+*/ Do While Loop
+int A=0;
+    do {
+        printf("Input a num from 1 to 100  : ");
+        scanf("%d",&A);
+       }
+
+    while (A<1 || A>100);
+    printf("Perfect : %d\n",A);
+
+*/
+
+
+
+
+
+
+
+
+return 0 ;     
 }
