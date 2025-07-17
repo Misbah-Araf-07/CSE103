7872826b9a9fc51a6dba256f227422c726397ae0
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-18 00:55:50 +0600

    Create Tells which Day is it of the week.c

---
+++ Statement Problems/Tells which Day is it of the week.c
@@ -0,0 +1,36 @@
+
+  method-1: using if else 
+
+
+#include <stdio.h>
+
+
+int main()
+{
+    int day;
+    printf("Please input what numbered day is it : ");
+    scanf("%d",&day);
+    if (day==1)
+        printf("It is Saturday :(\n");
+    else if (day==2)
+        printf("It is Sunday\n");
+    else if (day==3)
+        printf("It is Monday\n");
+    else if (day==4)
+        printf("It is Tuesday\n");
+    else if (day==5)
+        printf("It is Wednesday\n");
+    else if (day==6)
+        printf("It is Thursday\n");
+    else if (day==7)
+        printf("It is Friday!!\n");
+    else if (day==0)
+        printf("0th day doesn't exist\n");
+    else
+        printf("1 week = 7days\n");
+    return 0;
+}
+
+
+    method- 2 : using switch 
+
