83de569267cfa420408bca9777beafbcf21e54ae
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-07-26 23:35:02 +0600

    Update Calculate Cube of a number.c

--- Functions and Loops/Calculate Cube of a number.c
+++ Functions and Loops/Calculate Cube of a number.c
@@ -1,5 +1,7 @@
 //Write a C program to input any number from user and find cube of the given number using function.
 
+//Type 1 : For a int type input
+
 #include <stdio.h>
 int PrintCubic (int x);
 
@@ -16,6 +18,34 @@ int main ()
 }
 
 int PrintCubic(int value)
+{
+    return value*value*value;
+}
+
+
+
+//Type-2: For a float type input 
+
+
+//Write a C program to input any number from user and find cube of the given number using function.
+
+
+#include <stdio.h>
+double CaclulateCubic ();
+
+
+int main ()
+{
+    double A=0, cA=0;
+    printf("Please Enter a number : ");
+    scanf("%lf",&A);
+
+    cA=CaclulateCubic(A);
+    printf("Cubic of %.1lf is = %.1lf\n",A,cA);
+    return 0 ;
+}
+
+double CaclulateCubic(double value)
 {
     return value*value*value;
 }
