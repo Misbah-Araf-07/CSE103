2d7c5184c0250f6b6c43c9ee781e6890d98b56ae
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-08-19 16:27:56 +0600

    Create Take Book info and Show them .c

---
+++ Structure/Take Book info and Show them .c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Book
+{
+    char name[50],Author[50];
+    int page ;
+    float rating;
+};
+
+int main ()
+{
+    struct Book n1 ;
+    printf("Enter Book-Name, Author-name ,Page-amount,User-Rating:\n");
+    gets(n1.name);
+    gets(n1.Author);
+    scanf("%d %f", &n1.page, &n1.rating);
+
+    printf("Book info : \n");
+    printf("%s %s %d %f \n", n1.name, n1.Author, n1.page, n1.rating);
+
+    printf("\n");
+
+    return 0;
+
+
+
+
+
+
+
+}
+
