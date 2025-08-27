7ab44d341b6b0f0248c42668c0378c96d0f9efa7
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-08-27 11:57:01 +0600

    Create Problem K : Area of Red and Green of Flag .c

---
+++ Lab Files (Vjudge)/Problem K : Area of Red and Green of Flag .c
@@ -0,0 +1,3 @@
+//Given the flag’s length, calculate and output the area of the red circle and the remaining green area, with two decimal digits.
+
+Given NN lines to print, determine the minimum number of paste operations needed to produce a program printing exactly NN "Hello World!" lines.
