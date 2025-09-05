9e2694cef4f6b02083c97d6c5b69b9cd00572fd6
Misbah Ahmed Araf <arafallen0@gmail.com>
2025-09-05 21:23:20 +0600

    Create GCD using Euclidean algo.c

---
+++ Recursion/GCD using Euclidean algo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int main() {
+    int x = 48, y = 18;
+    printf("GCD of %d and %d is %d\n", x, y, gcd(x, y));
+    return 0;
+}
